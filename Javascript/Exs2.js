475553a3f17a4871a3bb0666253b45b57cf551d3
LadyFantasy <msoledadm88@gmail.com>
2020-05-09 17:08:14 +0100

    Exs2 JS

--- Javascript/Exs2.js
+++ Javascript/Exs2.js
@@ -1,39 +1,33 @@
 // FIZZBUZZ
 
 
-function fizzbuzz(number){
+function fizzbuzz(number) {
 
-    if(number!=NaN) {
-  
-      for (let i=1; i<=end;i++){
-        
-          if(i%3===0 && i%5===0){
-            console.log("Fizzbuzz");
-          }
-          
-          else if(i%3===0){
+    if (number != NaN) {
+
+        for (let i = 1; i <= end; i++) {
+
+            if (i % 3 === 0 && i % 5 === 0) {
+                console.log("Fizzbuzz");
+            } else if (i % 3 === 0) {
                 console.log("Fizz");
-            }
-      
-            else if(i%5===0){
+            } else if (i % 5 === 0) {
                 console.log("Buzz");
-            }
-              
-            else{
+            } else {
                 console.log(i);
             }
         }
-      }
-     
-  }
+    }
 
-  
+}
+
+
 //*******2. Crear una funcion que devuelva el mayor numero dentro de un array**********
 
 
 const arrayTest = [4, 7, 98, -6, 2, -23]
 
-function mayor(arr){
+function mayor(arr) {
     return Math.max(...arr)
 }
 
@@ -41,14 +35,16 @@ mayor(arrayTest)
 
 
 
- //3.Crear una funcion que devuelva los dos numeros mas grandes dentro de un array
+//3.Crear una funcion que devuelva los dos numeros mas grandes dentro de un array
 
- const arrayTest = [4, 7, 98, -6, 2, -23]
+const arrayTest = [4, 7, 98, -6, 2, -23]
 
 function dosMayores(arr) {
-const newArr = arr.sort((a,b) => {return a-b})
-console.log(newArr)
-console.log(newArr[newArr.length - 1] , newArr[newArr.length - 2])
+    const newArr = arr.sort((a, b) => {
+        return a - b
+    })
+    console.log(newArr)
+    console.log(newArr[newArr.length - 1], newArr[newArr.length - 2])
 }
 
 dosMayores(arrayTest)
@@ -57,15 +53,15 @@ dosMayores(arrayTest)
 
 //4.Crear una funcion que diga todos los elementos que aparecen mas de una vez en un array.
 
-const arr = [1,2,3,2,3,4,1]
+const arr = [1, 2, 3, 2, 3, 4, 1]
 
-function duplicados(array){
+function duplicados(array) {
     const noDuplis = []
     const duplis = []
 
-    array.map(elem =>{
+    array.map(elem => {
         noDuplis.includes(elem) ? duplis.push(elem) : noDuplis.push(elem)
-       })
+    })
     return duplis
 }
 
@@ -74,13 +70,22 @@ duplicados(arr)
 
 //5.Crear una funcion que diga todos los elementos que aparecen solo una vez en un array.
 
-const arr = [1,2,3,2,3,4,1]
 
-function unoSolo(array){
-   
+const arr = [1, 2, 3, 2, 3, 4, 1]
+
+function duplicados(array) {
+    const noDuplis = []
+    const duplis = []
+
+    array.map(elem => {
+        noDuplis.includes(elem) ? duplis.push(elem) && noDuplis.splice(noDuplis.indexOf(elem), 1): noDuplis.push(elem)
+    })
+
+    return noDuplis
 }
 
-unoSolo(arr)
+duplicados(arr)
+
 
 
 //6.Crear una funcion que determine si una palabra es palindromo o no.
@@ -91,6 +96,6 @@ function palindromo(word) {
 
 //7.Crear una funcion que devuelva el string que le enviamos pero invertido.
 
-function invertido(word){
+function invertido(word) {
     return word.split("").reverse().join("")
 }
