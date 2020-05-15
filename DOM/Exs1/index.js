8d0da125da5db976b11a23417637cee9811dd11a
LadyFantasy <msoledadm88@gmail.com>
2020-05-15 15:45:53 +0100

    Exs DOM

--- DOM/Exs1/index.js
+++ DOM/Exs1/index.js
@@ -1,13 +1,15 @@
-//////-Primer ejercicio
+///////Primer ejercicio
+
 const inputs = document.querySelectorAll(".inputText")
 
 
 const clean = () => {
-   inputs.forEach(input=>input.value="")
+    inputs.forEach(input => input.value = "")
 }
 
 
-///////Segundo y tercer ejercicios
+///////Segundo y tercer ejercicio
+
 const inputAdd = document.querySelector(".input-add")
 
 const list = document.querySelector(".inputs-li")
@@ -16,7 +18,7 @@ const addInput = () => {
     const li = document.createElement("li")
     list.appendChild(li)
     li.innerText = inputAdd.value
-    inputAdd.value = ""  
+    inputAdd.value = ""
 }
 
 const deleteInput = () => {
